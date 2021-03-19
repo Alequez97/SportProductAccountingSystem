2ae71cdb6338b4135cdbc12fde6aced51deedbe0
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2021-03-19 09:14:04 +0200

    Add project files

---
+++ inventory.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Inventory</title>
+        <link rel="stylesheet" href="styles/styles.css" >
+    </head>
+
+    <body>
+        <div class="container">
+            <h1 class="text-center">Inventory</h1>
+            <div class="border">
+                <div class="table-wrapper">
+                    <table class="text-center center">
+                        <thead>
+                            <tr>
+                                <th>
+                                    Name
+                                </th>
+                                <th>
+                                    Color
+                                </th>
+                                <th>
+                                    Quantity
+                                </th>
+                                <th>
+                                    Details
+                                </th>
+                                <th>
+                                    Action
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    Resistance band
+                                </td>
+                                <td>
+                                    Yellow
+                                </td>
+                                <td>
+                                    8
+                                </td>
+                                <td>
+                                    <a href="#">Details</a>
+                                </td>
+                                <td>
+                                    <button>Edit</button>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Resistance band
+                                </td>
+                                <td>
+                                    Yellow
+                                </td>
+                                <td>
+                                    8
+                                </td>
+                                <td>
+                                    <a href="#">Details</a>
+                                </td>
+                                <td>
+                                    <button>Edit</button>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Resistance band
+                                </td>
+                                <td>
+                                    Yellow
+                                </td>
+                                <td>
+                                    8
+                                </td>
+                                <td>
+                                    <a href="#">Details</a>
+                                </td>
+                                <td>
+                                    <button>Edit</button>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Resistance band
+                                </td>
+                                <td>
+                                    Yellow
+                                </td>
+                                <td>
+                                    8
+                                </td>
+                                <td>
+                                    <a href="#">Details</a>
+                                </td>
+                                <td>
+                                    <button>Edit</button>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </body>
+
+</html>
