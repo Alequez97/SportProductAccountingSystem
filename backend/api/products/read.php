616a175f33261904e5b20fcc44f1d4d23f35cdf2
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2021-03-20 16:39:29 +0200

    Add api endpoints for read update and delete

---
+++ backend/api/products/read.php
@@ -0,0 +1,29 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+
+    // include database
+    include_once("../../services/DatabaseWorkerFactory.php");
+    include_once("../../interfaces/IDatabaseConnection.php");
+    include_once("../../database/MySqlDatabaseConnection.php");
+    include_once("../../interfaces/IDatabaseWorker.php");
+    include_once("../../services/DatabaseWorker.php");
+  
+    // instantiate database and product object
+    $dbWorker = DatabaseWorkerFactory::GetMySqlDatabaseWorker("localhost", "root", "", "jurec_sanja");
+    $id = isset($_GET['id']) ? $_GET['id'] : die();
+    $product = $dbWorker->Read('products', $id);
+
+    if (isset($product))
+    {
+        http_response_code(200);
+        echo json_encode($product);
+    }
+    else
+    {
+        http_response_code(404);
+        echo json_encode(array("message" => "Product with id $id not found"));
+    }
+
+    
+?>
